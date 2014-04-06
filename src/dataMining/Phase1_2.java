c974648d5e4b1814282331827a1190ddd859395f
Trigunale <alexandre.cazala@gmail.com>
2014-04-06 23:14:19 +0200

    Phase 1 partie 1 Dico

--- src/dataMining/Phase1_2.java
+++ src/dataMining/Phase1_2.java
@@ -1,16 +1,24 @@
 package dataMining;
 import java.io.*;
 import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.StringTokenizer;
 
 public class Phase1_2 {
-    public static void generateTransFile(String transPath, String csvPath) throws IOException {
-        File file = new File(csvPath);
+    public static void generateTransFile(String csvPath, String dicoPath, String transPath) throws IOException {
+        // notre CSV
+    	File file = new File(csvPath + ".csv");
         BufferedReader bufferedReader = new BufferedReader( new FileReader(file) );
-        File transFile = new File(transPath);
-		System.out.println("fichier crée");
-		transFile.createNewFile();
-		FileWriter fw = new FileWriter(transFile);
+        // notre trans
+        File transFile = new File(transPath + ".trans");
+        if(!transFile.exists()) {
+            transFile.createNewFile();
+        }
+        BufferedWriter bwTrans = new BufferedWriter( new FileWriter(transFile) );
+		// notre dictionnaire
+		File dicoFile = new File(dicoPath + ".txt");
+		System.out.println("fichier dico crée");
+		BufferedWriter bwDico = new BufferedWriter( new FileWriter(dicoFile) );
 		String strTrans = new String();
         String line, token;
         StringTokenizer stringTokenizer;
@@ -31,23 +39,29 @@ public class Phase1_2 {
             strTrans += "\n";
         }
 
-        if(!transFile.exists()) {
-            transFile.createNewFile();
+        for(Entry<String, Integer> entry : hashMap.entrySet()) {
+            String cle = entry.getKey();
+            Integer valeur = entry.getValue();
+            bwDico.write(cle + ";" + valeur);
+            bwDico.newLine();
         }
-
-        fw.write(strTrans);
-        fw.flush();
-        fw.close();
+        bwDico.flush();
+        bwDico.close();
+        bwTrans.write(strTrans);
+        bwTrans.flush();
+        bwTrans.close();
         bufferedReader.close();
     }
     public static void main(String[] args) throws IOException {
-    	if ( 2 != args.length) {
-			System.out.println("Erreur : nombre d'arguments invalide\nArguments attendus : CheminInCSV CheminOutTrans");
+    	if ( 3 != args.length) {
+			System.out.println("Erreur : nombre d'arguments invalide\nArguments attendus : CheminInCSV CheminOutDico CheminOutTrans");
 			return;
 		}
     	String csvPath = args[0];
-    	String transPath = args[1];
-    	generateTransFile(transPath,csvPath);
+    	String dicoPath = args[1];
+    	String transPath = args[2];
     	
+    	generateTransFile(csvPath, dicoPath, transPath);
+    	System.out.println("\n---\nFin du traitement\n");
     }
 }
